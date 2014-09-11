ce781d0bb3e20fc6d7dd6a267b357d9e58e87647
linxcat <clavoy_comp@hotmail.com>
2014-09-10 20:01:48 -0600

    Dash logic fixes
    Using Warren's advice, fixed dash to only take place on button down,
    allowing me to remove extraneous "anti-spam" cooldown.

--- Movement.cs
+++ Movement.cs
@@ -7,11 +7,9 @@ public class Movement : MonoBehaviour {
 	private float JUMP_FORCE; // strength of the impulse used to launch the player on a jump
 	private float PUSH_HEIGHT; // abstract height above the ground where players can be considered "on the ground"
 	private float DASH_STRENGTH; // stregth of impulse moving character laterally on a dash (to be replaced)
-	private float SPAM_COOLDOWN; // number of seconds to wait between consecutive dashes
 	private int USEABLE = -1; // readable value for cooldowns that are complete
 	private float cooldown1; // 1st dash cooldown
 	private float cooldown2; // 2nd dash cooldown
-	private float spamcooler; // dash-spam cooldown
 
 	// Use this for initialization
 	void Start ()
@@ -20,10 +18,8 @@ public class Movement : MonoBehaviour {
 		JUMP_FORCE = 10f;
 		PUSH_HEIGHT = 0.4f;
 		DASH_STRENGTH = 6f;
-		SPAM_COOLDOWN = 0.75f;
 		cooldown1 = USEABLE;
 		cooldown2 = USEABLE;
-		spamcooler = USEABLE;
 
 	}
 
@@ -39,13 +35,13 @@ public class Movement : MonoBehaviour {
 		if ( IsGrounded() && Input.GetKey(KeyCode.UpArrow) )
 			rigidbody2D.AddForce(Vector2.up * JUMP_FORCE, ForceMode2D.Impulse);
 
-		if ( Input.GetKey(KeyCode.D) && CanDash() )
+		if ( Input.GetKeyDown(KeyCode.D) && CanDash() )
 		{
 			RemoveDashMarker();
 			rigidbody2D.AddForce(Vector2.right * DASH_STRENGTH, ForceMode2D.Impulse);
 		}
 
-		if ( Input.GetKey(KeyCode.A) && CanDash() )
+		if ( Input.GetKeyDown(KeyCode.A) && CanDash() )
 		{
 			RemoveDashMarker();
 			rigidbody2D.AddForce(-Vector2.right * DASH_STRENGTH, ForceMode2D.Impulse);
@@ -69,17 +65,14 @@ public class Movement : MonoBehaviour {
 			cooldown1 = USEABLE;
 		if ( USEABLE != cooldown2 && cooldown2 <= Time.time)
 			cooldown2 = USEABLE;
-		if (USEABLE != spamcooler && spamcooler <= Time.time)
-			spamcooler = USEABLE;
-		return ( USEABLE == spamcooler && (USEABLE == cooldown2 || USEABLE == cooldown1) );
+		return (USEABLE == cooldown2 || USEABLE == cooldown1);
 	}
 
-	// sets one of the dash cooldowns (and the spam cooldown) before a dash.
+	// sets one of the dash cooldowns before a dash.
 	// this function assumes that you have already checked to see if a dash is available.
 	// it will consume dashes from right to left (i.e. 2nd, then 1st)
 	private void RemoveDashMarker()
 	{
-		spamcooler = Time.time + SPAM_COOLDOWN;
 		if ( USEABLE != cooldown2 )
 			cooldown1 = Time.time + 5;
 		else
