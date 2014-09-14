f9e2cd94d75d78527bccd6a7596d17236bf48fc2
linxcat <clavoy_comp@hotmail.com>
2014-09-13 19:34:14 -0600

    Fully implemented leveling for all player movement and moved all stats to the PlayersStats script

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -2,47 +2,26 @@ using UnityEngine;
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-
-	//Moving
-	private float MOVE_SPEED; // the lateral speed of the players (as a force for air-control)
-
-	//Jumping
-	private float JUMP_IMPULSE; // strength of the impulse used to launch the player on a jump
-	private float JUMP_FORCE; // constant force value that player experiences while jumping
-	private float PUSH_HEIGHT; // abstract height above the ground where players can be considered "on the ground"
-	private bool JUMP_EXPIRED = true; // whether or not the player must wait to hit the ground before jumping again
-
-	//Dashing
-	private float DASH_POWER; // speed at which the dash is performed (inverse to WAIT_TIME)
-	private float WAIT_TIME; // time after dash to wait before turning physics back on
-	private const int USEABLE = -1; // human-readable value for cooldowns that are complete
-	private float dashCooldown1; // 1st dash cooldown timestamp
-	private float dashCooldown2; // 2nd dash cooldown timestamp
-	private float dashWait; // timestamp to wait till dash completes
+	private PlayerData playerData;
 
 	// Use this for initialization
 	void Start ()
 	{
-		MOVE_SPEED = 10f;
-
-		JUMP_IMPULSE = 4f;
-		JUMP_FORCE = 7f;
-		PUSH_HEIGHT = 0.4f;
-
-		DASH_POWER = 100f;
-		WAIT_TIME = 0.05f;
-		dashCooldown1 = USEABLE;
-		dashCooldown2 = USEABLE;
-		dashWait = USEABLE;
+		playerData = GetComponent<PlayerData>();
 	}
 
 	// Update is called once per frame (used for non-physics)
 	void Update()
 	{
-		if (USEABLE != dashWait && dashWait <= Time.time)
+		if (playerData.IsDashOver())
 		{
 			StartPhysics();
-			dashWait = USEABLE;
+			playerData.ClearWaitTimer();
+		}
+
+		if (Input.GetKeyDown(KeyCode.J))
+		{
+			playerData.LevelUp(PlayerData.Attribute.Dash, 3);
 		}
 	}
 
@@ -50,25 +29,25 @@ public class Movement : MonoBehaviour {
 	void FixedUpdate ()
 	{
 		if ( Input.GetKeyUp(KeyCode.UpArrow) || rigidbody2D.velocity.y < 0)
-			JUMP_EXPIRED = true;
+			playerData.SetJumpable(true);
 
 		if ( Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded() )
 			Jump();
 
-		if ( Input.GetKey(KeyCode.UpArrow) && !JUMP_EXPIRED )
-			rigidbody2D.AddForce(Vector2.up * JUMP_FORCE);
+		if ( Input.GetKey(KeyCode.UpArrow) && playerData.CanJump() )
+			rigidbody2D.AddForce(Vector2.up * playerData.GetJUMP_FORCE());
 
-		if ( Input.GetKeyDown(KeyCode.D) && CanDash() )
+		if ( Input.GetKeyDown(KeyCode.D) && playerData.CanDash() )
 			DashRight();
 
-		if ( Input.GetKeyDown(KeyCode.A) && CanDash() )
+		if ( Input.GetKeyDown(KeyCode.A) && playerData.CanDash() )
 			DashLeft();
 
 		if ( Input.GetKey(KeyCode.LeftArrow) )
-			rigidbody2D.AddForce(-Vector2.right * MOVE_SPEED);
+			rigidbody2D.AddForce(-Vector2.right * playerData.GetMOVE_SPEED());
 
 		if ( Input.GetKey(KeyCode.RightArrow) )
-			rigidbody2D.AddForce(Vector2.right * MOVE_SPEED);
+			rigidbody2D.AddForce(Vector2.right * playerData.GetMOVE_SPEED());
 	}
 
 	// Stop rigidbody motion (for a dash)
@@ -90,58 +69,36 @@ public class Movement : MonoBehaviour {
 	// Dash the player to the left
 	private void DashLeft()
 	{
-		RemoveDashMarker();
+		playerData.RemoveDashMarker();
 		StopPhysics();
-		rigidbody2D.velocity = (-Vector2.right * DASH_POWER);
-		dashWait = Time.time + WAIT_TIME;
+		rigidbody2D.velocity = (-Vector2.right * playerData.GetDASH_POWER());
+		playerData.SetWaitTimer();
 	}
 
 	// Dash the player to the right
 	private void DashRight()
 	{
-		RemoveDashMarker();
+		playerData.RemoveDashMarker();
 		StopPhysics();
-		rigidbody2D.velocity = (Vector2.right * DASH_POWER);
-		dashWait = Time.time + WAIT_TIME;
-	}
-
-	// Does the player have an available dash?
-	// returns: true if there is one or more dashes available
-	private bool CanDash()
-	{
-		if ( USEABLE != dashCooldown1 && dashCooldown1 <= Time.time)
-			dashCooldown1 = USEABLE;
-		if ( USEABLE != dashCooldown2 && dashCooldown2 <= Time.time)
-			dashCooldown2 = USEABLE;
-		return (USEABLE == dashCooldown2 || USEABLE == dashCooldown1);
-	}
-
-	// sets one of the dash cooldowns before a dash.
-	// this function assumes that you have already checked to see if a dash is available.
-	// it will consume dashes from right to left (i.e. 2nd, then 1st)
-	private void RemoveDashMarker()
-	{
-		if ( USEABLE != dashCooldown2 )
-			dashCooldown1 = Time.time + 5;
-		else
-			dashCooldown2 = Time.time + 5;
+		rigidbody2D.velocity = (Vector2.right * playerData.GetDASH_POWER());
+		playerData.SetWaitTimer();
 	}
 
 	// is the player on the ground (and able to jump)?
 	// returns: true if player is close enough to the ground
 	private bool IsGrounded()
 	{
-		RaycastHit2D result = Physics2D.Raycast(this.transform.position, -Vector2.up, PUSH_HEIGHT);
+		RaycastHit2D result = Physics2D.Raycast(transform.position, -Vector2.up, playerData.PUSH_HEIGHT);
 		bool grounded = null != result.collider;
 		if (grounded)
-			JUMP_EXPIRED = false;
+			playerData.SetJumpable(false);
 		return (grounded);
 	}
 
 	//Make the player jump
 	private void Jump()
 	{
-		rigidbody2D.AddForce(Vector2.up * JUMP_IMPULSE, ForceMode2D.Impulse);
+		rigidbody2D.AddForce(Vector2.up * playerData.GetJUMP_IMPULSE(), ForceMode2D.Impulse);
 	}
 
 }
--- Scripts/PlayerData.cs
+++ Scripts/PlayerData.cs
@@ -1,26 +1,211 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class PlayerData : MonoBehaviour 
+public class PlayerData : MonoBehaviour
 {
+	//---Structs and Enums----------------------------------------------------------------------------------------------
 
-	private int MoneyAmount; // The amount of money a player has
+	//Structs and enums
+	public enum Attribute { Move, Jump, Dash, Money }; //giving readable names to pass-in values 0-4
 
-	// Use this for initialization
-	void Start () 
+	private struct MoveLevel
 	{
-		MoneyAmount = 500;
+		public static float[] moveSpeed = new float[3] { 10f, 12f, 15f };
 	}
 
+	private struct JumpLevel
+	{
+		public static float[] impulse = new float[3] { 4f, 5f, 6f };
+		public static float[] force = new float[3] { 7f, 8f, 9f };
+	}
+	
+	private struct DashLevel
+	{
+		public static float[] powerLevel = new float[3] { 25f, 50f, 100f };
+		public static float[] waitTime = new float[3] { 0.2f, 0.1f, 0.05f };
+	}
+
+	//---Custom Classes-------------------------------------------------------------------------------------------------
+
+	//---Attribute Variables---------------------------------------------------------------------------------------------------
+
+	//Moving
+	private float MOVE_SPEED; // the lateral speed of the players (as a force for air-control)
+
+	//Jumping
+	private float JUMP_IMPULSE; // strength of the impulse used to launch the player on a jump
+	private float JUMP_FORCE; // force value that player experiences while jumping
+	public readonly float PUSH_HEIGHT = 0.4f; // abstract height above the ground where players can be considered "on the ground"
+	private bool JUMP_EXPIRED; // whether or not the player must wait to hit the ground before jumping again
+
+	//Dashing
+	private float DASH_POWER; // speed at which the dash is performed (inverse to WAIT_TIME)
+	private float WAIT_TIME; // time after dash to wait before turning physics back on
+	private readonly int USEABLE = -1; // human-readable value for cooldowns that are complete
+	private float dashCoolTime;
+	private float dashCooldown1; // 1st dash cooldown timestamp
+	private float dashCooldown2; // 2nd dash cooldown timestamp
+	private float dashWait; // timestamp to wait till dash completes
+
+	//Money
+	private int moneyAmount; // The amount of money a player has
+
+	//---Unity Functions------------------------------------------------------------------------------------------------
+
+	// Use this for initialization
+	void Start ()
+	{
+		MOVE_SPEED = 10f;
+
+		JUMP_IMPULSE = 4f;
+		JUMP_FORCE = 7f;
+		JUMP_EXPIRED = true;
+
+		DASH_POWER = 25f;
+		WAIT_TIME = 0.2f;
+		dashCoolTime = 5f;
+		dashCooldown1 = USEABLE;
+		dashCooldown2 = USEABLE;
+		dashWait = USEABLE;
+
+		moneyAmount = 500;
+	}
+
+	//---Custom Functions-----------------------------------------------------------------------------------------------
+
+	// functions to automatically call-up and set player stats to pre-defined level ranges
+	public void LevelUp (Attribute attrib, int level)
+	{
+		switch (attrib)
+		{
+			case Attribute.Move :
+			{
+				MOVE_SPEED = MoveLevel.moveSpeed[level-1];
+				return;
+			}
+
+			case Attribute.Jump :
+			{
+				JUMP_IMPULSE = JumpLevel.impulse[level-1];
+				JUMP_FORCE = JumpLevel.force[level-1];
+				return;
+			}
+
+			case Attribute.Dash :
+			{
+				
+				DASH_POWER = DashLevel.powerLevel[level-1];
+				WAIT_TIME = DashLevel.waitTime[level-1];
+				return;
+			}
+
+			case Attribute.Money :
+			{
+				ChangeMoney(level);
+				return;
+			}
+		}
+	}
+
+	public bool CanJump()
+	{
+		return !JUMP_EXPIRED;
+	}
+
+	public void SetJumpable (bool setting)
+	{
+		JUMP_EXPIRED = setting;
+	}
+
+	public void SetWaitTimer ()
+	{
+		dashWait = Time.time + WAIT_TIME;
+	}
+
+	public void SetCooldown1()
+	{
+		dashCooldown1 = Time.time + dashCoolTime;
+	}
+
+	public void SetCooldown2 ()
+	{
+		dashCooldown2 = Time.time + dashCoolTime;
+	}
+
+	public void ClearWaitTimer ()
+	{
+		dashWait = USEABLE;
+	}
+
+	public void ClearCooldown1 ()
+	{
+		dashCooldown1 = USEABLE;
+	}
+
+	public void ClearCooldown2 ()
+	{
+		dashCooldown2 = USEABLE;
+	}
+
+	public float GetMOVE_SPEED ()
+	{
+		return MOVE_SPEED;
+	}
+
+	public float GetJUMP_IMPULSE ()
+	{
+		return JUMP_IMPULSE;
+	}
+
+	public float GetJUMP_FORCE ()
+	{
+		return JUMP_FORCE;
+	}
+
+	public float GetDASH_POWER ()
+	{
+		return DASH_POWER;
+	}
+
+	public bool IsDashOver ()
+	{
+		return USEABLE != dashWait && dashWait <= Time.time;
+	}
+	
+	// Does the player have an available dash?
+	// returns: true if there is one or more dashes available
+	public bool CanDash()
+	{
+		if (USEABLE != dashCooldown1 && dashCooldown1 <= Time.time)
+			ClearCooldown1();
+		if (USEABLE != dashCooldown2 && dashCooldown2 <= Time.time)
+			ClearCooldown2();
+		return (USEABLE == dashCooldown2 || USEABLE == dashCooldown1);
+	}
+
+	// sets one of the dash cooldowns before a dash.
+	// this function assumes that you have already checked to see if a dash is available.
+	// it will consume dashes from right to left (i.e. 2nd, then 1st)
+	public void RemoveDashMarker()
+	{
+		if (USEABLE != dashCooldown2)
+			if (USEABLE != dashCooldown1)
+				print("Dash Overconsumption");
+			else
+			SetCooldown1();
+		else
+			SetCooldown2();
+	}
+	
 	// allows shops to see the amount of money a player has.
 	public int GetMoney()
 	{
-		return this.MoneyAmount;
+		return this.moneyAmount;
 	}
 	
 	// subtracts or adds the amount of money specified to the player's moneyAmount.
 	public void ChangeMoney(int change)
 	{
-		MoneyAmount = MoneyAmount + change;
+		moneyAmount = moneyAmount + change;
 	}
 }
