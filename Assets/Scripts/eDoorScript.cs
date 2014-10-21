97bc6d5eb8a1046b9f91ae09a02ee1f0771820d5
SwiftSwrd <savage_sw450@hotmail.com>
2014-10-20 21:21:45 -0600

    Proc Gen
    Demo of proc gen for tomorrow.

---
+++ Assets/Scripts/eDoorScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eDoorScript : MonoBehaviour {
+
+	private GameObject ohGodHackJob;
+	public GameObject ohGodHackJob1;
+	public GameObject ohGodHackJob2;
+	private bool activated;
+
+	// Use this for initialization
+	void Start () {
+		activated = false;
+		float randNum = Random.value;
+		if(0.51 < randNum){
+			ohGodHackJob = ohGodHackJob1;
+		} else {
+			ohGodHackJob = ohGodHackJob2;
+		}
+	}
+
+	void OnTriggerEnter2D() {
+		if(!activated){
+			Instantiate (ohGodHackJob, new Vector3(transform.position.x+3.2f, transform.position.y), transform.rotation);
+			activated = true;
+		}
+	}
+}
