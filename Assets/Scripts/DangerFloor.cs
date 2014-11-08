aaf2b1728144b0d454eff6bc9bf53601145391bf
linxcat <clavoy_comp@hotmail.com>
2014-11-08 12:04:35 -0600

    Spiked floors/lava script
    Simply needs to be attached to an appropriate gameObject with a
    trigger(and a real hitbox to stop bullets)

---
+++ Assets/Scripts/DangerFloor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DangerFloor : MonoBehaviour {
+
+	private float DAMAGE;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag != "Projectile") //projectiles are all that move that can't be hurt
+		{
+			other.GetComponent<Actor>().StunDamage(DAMAGE, other.transform.position.x - transform.position.x >= 0f); //move shorter distance to edge of spikes
+		}
+	}
+}
