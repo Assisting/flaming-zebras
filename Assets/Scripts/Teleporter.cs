11bce92e86447e60de6ab283068dc6e76397ba9d
linxcat <clavoy_comp@hotmail.com>
2014-11-17 17:00:52 -0600

    cave level design prefabs

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public Transform shop; //the location of the shop teleporter
+
+	// Use this for initialization
+	void Start () {
+		shop = GameObject.Find("HomeTeleport").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/floorTiling.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floorTiling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.SetTextureScale("_MainTex", new Vector2(transform.lossyScale.x, 1f));
+	}
+}
