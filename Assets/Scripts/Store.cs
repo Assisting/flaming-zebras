0e602e183023a26c7327887a261e86d702de7505
linxcat <clavoy_comp@hotmail.com>
2014-11-21 14:25:56 -0600

    Getting shop rent

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -1,10 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Store : MonoBehaviour {
+
+	private int RENT_COST = 20;
+	private float GRACE = 25f;
+
+	private LinkedList<Rent> rents;
+
+	void Start()
+	{
+		patrons = new LinkedList<Rent>();
+	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void onTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			PlayerData script = other.GetComponent<PlayerData>();
+			script.CURLIFE = script.MAXLIFE; //full heal
+			rents.AddLast(new Rent(other.gameObject));
+			InvokeRepeating("Rent", GRACE, 1f);
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			CancelInvoke("Rent");
+		}
+	}
+
+	private void Rent()
+	{
+		foreach (GameObject patron in patrons)
+		{
+			
+		}
 	}
 }
