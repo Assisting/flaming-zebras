98c44fc1964b095b38bb5c8448ae8f1f82914955
linxcat <clavoy_comp@hotmail.com>
2014-11-20 18:08:32 -0600

    Use button begins
    renamed inputs first

--- Assets/Scripts/Characters/KeyBindings.cs
+++ Assets/Scripts/Characters/KeyBindings.cs
@@ -5,7 +5,7 @@ public class KeyBindings : MonoBehaviour {
 
 	//Keybindings
 	private string ATTACK_BUTTON; //mapping for attack/shoot button
-	private string BUY_BUTTON; //mapping for buy button
+	private string USE_BUTTON; //mapping for "USE" button
 	private string SHOW_MONEY_BUTTON; //mapping for showing the amoun of money you have. 
 										// hidden for screen-peeking reasons
 	private string PLAYER_X_AXIS; //mapping for thumbstick
@@ -24,7 +24,7 @@ public class KeyBindings : MonoBehaviour {
 			case 1:
 			{
 				ATTACK_BUTTON = "P1Fire1";
-				BUY_BUTTON = "P1Buy";
+				USE_BUTTON = "P1Use";
 				SHOW_MONEY_BUTTON = "P1ShowMoney";
 				PLAYER_X_AXIS = "P1Horizontal";
 				JUMP_BUTTON = "P1Jump";
@@ -35,7 +35,7 @@ public class KeyBindings : MonoBehaviour {
 			case 2:
 			{
 				ATTACK_BUTTON = "P2Fire1";
-				BUY_BUTTON = "P2Buy";
+				USE_BUTTON = "P2Use";
 				SHOW_MONEY_BUTTON = "P2ShowMoney";
 				PLAYER_X_AXIS = "P2Horizontal";
 				JUMP_BUTTON = "P2Jump";
@@ -46,7 +46,7 @@ public class KeyBindings : MonoBehaviour {
 			case 3:
 			{
 				ATTACK_BUTTON = "P3Fire1";
-				BUY_BUTTON = "P3Buy";
+				USE_BUTTON = "P3Use";
 				SHOW_MONEY_BUTTON = "P3ShowMoney";
 				PLAYER_X_AXIS = "P3Horizontal";
 				JUMP_BUTTON = "P3Jump";
@@ -57,7 +57,7 @@ public class KeyBindings : MonoBehaviour {
 			case 4:
 			{
 				ATTACK_BUTTON = "P4Fire1";
-				BUY_BUTTON = "P4Buy";
+				USE_BUTTON = "P4Use";
 				SHOW_MONEY_BUTTON = "P4ShowMoney";
 				PLAYER_X_AXIS = "P4Horizontal";
 				JUMP_BUTTON = "P4Jump";
@@ -78,9 +78,9 @@ public class KeyBindings : MonoBehaviour {
 		return ATTACK_BUTTON;
 	}
 
-	public string BuyButton()
+	public string UseButton()
 	{
-		return BUY_BUTTON;
+		return USE_BUTTON;
 	}
 
 	public string ShowMoneyButton()
