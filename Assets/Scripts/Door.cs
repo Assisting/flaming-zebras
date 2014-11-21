41565c21de35e1a380be2211e61fb7de6375a39c
linxcat <clavoy_comp@hotmail.com>
2014-11-20 22:01:57 -0600

    Doors done
    Build away!!

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public Transform pairExit;
+	public Transform thisExit;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			other.transform.position = pairExit.position;
+		}
+	}
+}
