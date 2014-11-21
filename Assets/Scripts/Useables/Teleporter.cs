4b7a082d8481092774cc72ec9bed3cd9b3ee686b
linxcat <clavoy_comp@hotmail.com>
2014-11-20 21:44:51 -0600

    Teleporters will now function
    naming stuff

--- Assets/Scripts/Useables/Teleporter.cs
+++ Assets/Scripts/Useables/Teleporter.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class Teleporter : Usable{
 
-	private Vector2 homeTeleport; //the location of the shop teleporter
+	private Vector2 rootTeleport; //the location of the shop teleporter
 
 	// Use this for initialization
 	void Start () {
-		homeTeleport = GameObject.Find("HomeTeleport").transform.position;
+		rootTeleport = GameObject.Find("RootTeleporter").transform.position;
 	}
 	
 	public override void Use(GameObject caller)
 	{
 		caller.GetComponent<PlayerData>().setLastTeleport(transform.position);
-		caller.transform.position = homeTeleport;
+		caller.transform.position = rootTeleport;
 	}
 }
