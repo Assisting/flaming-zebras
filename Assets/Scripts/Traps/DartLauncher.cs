2c674b9f71d9c05170882c0bbab9c615eea2825c
linxcat <clavoy_comp@hotmail.com>
2014-11-20 23:09:55 -0600

    Dart Launcher Done

---
+++ Assets/Scripts/Traps/DartLauncher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DartLauncher : Trap {
+
+	public Rigidbody2D dart;
+	public Transform muzzle;
+
+	public override void Activate()
+	{
+		Instantiate(dart, muzzle.position, muzzle.rotation);
+	}
+}
