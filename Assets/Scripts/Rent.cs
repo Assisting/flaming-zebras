2e4440d6ae46223e0c9cd21d82df4d5c35e19814
linxcat <clavoy_comp@hotmail.com>
2014-11-22 16:43:18 -0600

    Store Rent implemented
    put it on the thing with the giant trigger box, and test

---
+++ Assets/Scripts/Rent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rent : MonoBehaviour {
+
+	private GameObject patron;
+
+	private int RENT_COST = 20;
+	private float GRACE = 25f;
+
+	void Awake()
+	{
+		this.patron = patron;
+		InvokeRepeating("RentPay", GRACE, 1f);
+	}
+
+	public void SetPatron(GameObject patron)
+	{
+		this.patron = patron;
+	}
+
+	private void RentPay()
+	{
+		patron.GetComponent<PlayerData>().ChangeMoney(-RENT_COST);
+	}
+
+	public void StopPay()
+	{
+		CancelInvoke("RentPay");
+	}
+}
---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Store : MonoBehaviour {
+
+	private Dictionary<GameObject, Rent> rents;
+
+	void Start()
+	{
+		rents = new Dictionary<GameObject, Rent>();
+	}
+
+	void onTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			PlayerData script = other.GetComponent<PlayerData>();
+			script.CURLIFE = script.MAXLIFE; //full heal
+			Rent newRent = new Rent();
+			newRent.SetPatron(other.gameObject);
+			rents.Add(other.gameObject, newRent);
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			Rent removeRent = null;
+			rents.TryGetValue(other.gameObject, out removeRent);
+			if (removeRent != null)
+				removeRent.StopPay();
+			rents.Remove(other.gameObject);
+		}
+	}
+}
+
