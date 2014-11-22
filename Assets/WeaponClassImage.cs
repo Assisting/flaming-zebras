c26ebfb62c9f1b2c771b2add545395f4ba8d98d1
Kraysus <syuveil316@yahoo.ca>
2014-11-21 20:25:27 -0600

    stuff

--- Assets/WeaponClassImage.cs
+++ Assets/WeaponClassImage.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class WeaponClassImage : MonoBehaviour {
 
 	public PlayerData playerData;
+	public Texture2D bomb;
+	public Texture2D bullet;
+	public Texture2D missile;
+	public Texture2D sword;
+	public Texture2D laser;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +18,17 @@ public class WeaponClassImage : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		switch (playerData.GetWeaponType())
-			case Weapon.WeaponType.Bomb;
+		{
+		case Weapon.WeaponType.Bomb:
+		{
+			guiTexture.texture = bomb;
+			break;
+		}
+		case Weapon.WeaponType.Bullet:
+		{
+			guiTexture.texture = bullet;
+			break;
+		}
+		}
 	}
 }
