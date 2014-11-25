a8feca9f6b4873ab5203d9d3078a08e7d718d3aa
linxcat <clavoy_comp@hotmail.com>
2014-11-25 14:46:44 -0600

    boulders kill you when they land on you
    and they are hard to move

--- Assets/Scripts/Traps/FallingBoulder.cs
+++ Assets/Scripts/Traps/FallingBoulder.cs
@@ -7,4 +7,21 @@ public class FallingBoulder : Trap {
 	{
 		rigidbody2D.isKinematic = false;
 	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!rigidbody2D.isKinematic)
+		{
+			if (other.tag == "Player")
+			{
+				PlayerData script = other.GetComponent<PlayerData>();
+				script.LifeChange(-script.MAXLIFE);
+			}
+			else if (other.tag == "Enemy")
+			{
+				Actor script = other.GetComponent<Actor>();
+				script.LifeChange(-script.MAXLIFE);
+			}
+		}
+	}
 }
