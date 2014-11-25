0810ad710e457a250c9933ed11d0ff0422d719b9
linxcat <clavoy_comp@hotmail.com>
2014-11-25 14:24:42 -0600

    spikes less shitty now
    they'll actually hit you

--- Assets/Scripts/LevelGenerate.cs
+++ Assets/Scripts/LevelGenerate.cs
@@ -6,9 +6,31 @@ public class LevelGenerate : MonoBehaviour {
 	private float HORIZONTAL_SEPARATION;
 	private float VERTICAL_SEPARATION;
 
+	private GameObject pastRoom;
+
+//	caveRoom1
+//	caveRoom2
+//	caveRoom3
+//		caveRoom10 caveRoom4
+//	caveRoom12
+//	caveRoom11
+//	caveRoom14
+
 	void Awake()
 	{
-		Application.LoadLevelAdditive("nameHere");
-		GameObject newLevel = GameObject.FindWithTag("CaveLevel"); 
+//		Door[] doors;
+//		
+//		Application.LoadLevelAdditive("storeRoom");
+//		GameObject newLevel = GameObject.FindWithTag("UnusedLevel");
+//		newLevel.transform.position += Vector3.up*VERTICAL_SEPARATION;
+//		pastRoom = newLevel;
+//		Application.LoadLevelAdditive("originRoom");
+//		newLevel = GameObject.FindWithTag("UnusedLevel");
+//		doors = pastRoom.GetComponentsInChildren<Door>();
+//		foreach(Door door in doors)
+//		{
+//			door.pairExit = tra
+//		}
+		
 	}
 }
