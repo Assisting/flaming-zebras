35f7696f0e5b4cca3506e9166a1ce2655d22f95c
linxcat <clavoy_comp@hotmail.com>
2014-11-30 16:31:18 -0600

    getting kicked hides the shop GUIText

--- Assets/Scripts/Rent.cs
+++ Assets/Scripts/Rent.cs
@@ -12,28 +12,32 @@ public class Rent : MonoBehaviour {
 	void Start()
 	{
 		playerData = GetComponent<PlayerData>();
-		playerData.MakeInvuln(Mathf.Infinity);
+		playerData.MakeInvuln(Mathf.Infinity); //invuln while in shop
 		InvokeRepeating("RentPay", GRACE, TICKTIME);
 	}
 
 	private void RentPay()
 	{
-		if (playerData.GetMoney() >= RENT_COST)
-			playerData.ChangeMoney(-RENT_COST);
+		if (playerData.GetMoney() >= RENT_COST) //if we have enough to pay rent
+			playerData.ChangeMoney(-RENT_COST); //pay
 		else
-		{
-			transform.position = new Vector3(0f, 0f, 0f);
-			playerData.SetTeleportCooldown();
-			playerData.MakeInvuln(RootTeleporter.teleInvulnTime);
-			if (playerData.GetWeaponType() == Weapon.WeaponType.None)
-				playerData.LevelUp(PlayerData.Attribute.WeaponType, Weapon.WeaponType.RUBBER_CHICKEN);
-			StopPay();
-		}
+			KickOut();
 	}
 
 	public void StopPay()
 	{
 		CancelInvoke("RentPay");
 		Destroy(this);
+	}
+
+	private void KickOut()
+	{
+		transform.position = new Vector3(0f, 0f, 0f); //move out of shop
+		playerData.SetTeleportCooldown(); //can't come back for a while
+		playerData.MakeInvuln(RootTeleporter.teleInvulnTime); //normal leaving invuln
+		transform.parent.Find("GUIText").Find("Shop").gameObject.layer = LayerMask.NameToLayer ("NoGUI"); //turn off the shopGUI, just in case
+		if (playerData.GetWeaponType() == Weapon.WeaponType.None) //if they didn't pick a weapon
+			playerData.LevelUp(PlayerData.Attribute.WeaponType, Weapon.WeaponType.RUBBER_CHICKEN); //give them the secret one
+		StopPay(); //stop the rent payment
 	}
 }
