64120845d7406f557bcc1a5e59654cb9eff6544b
linxcat <clavoy_comp@hotmail.com>
2014-12-03 00:08:20 -0600

    can get best players number
    just put it in a layer name for winner display

--- Assets/Scripts/Initialization/RunGame.cs
+++ Assets/Scripts/Initialization/RunGame.cs
@@ -7,6 +7,8 @@ public class RunGame : MonoBehaviour {
 	private int GAMETIME = 500; //game time in seconds
 	private int secondsPassed = 0;
 
+	private int bestPlayerNum = -1;
+
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating("GameTick", 1, 1); //game time goes up every second
@@ -47,10 +49,15 @@ public class RunGame : MonoBehaviour {
 			{
 				bestPlayer = player;
 				highestMoney = thisMoney;
-			}
-				
+			}	
 		}
+		bestPlayerNum = bestPlayer.GetComponent<PlayerData>().GetPlayerNum();
 		print("Congratulations " + bestPlayer.name + "! you won with a total of " + highestMoney + " gold."); //print ending message
 		Time.timeScale = 0f;//stop game
+	}
+
+	public int GetBestPlayer()
+	{
+		return bestPlayerNum;
 	}
 }
