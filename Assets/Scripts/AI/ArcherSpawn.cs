a373b1eaa34e62cbfd05a6fb59647e12d3d3e1a1
Assisting <ejl389@mail.usask.ca>
2014-12-03 11:16:04 -0600

    Adding some AI fixes and also mandatory credits
    Since both the legal system and our professor stated we need to credit
    the artists from the internet.

--- Assets/Scripts/AI/ArcherSpawn.cs
+++ Assets/Scripts/AI/ArcherSpawn.cs
@@ -5,8 +5,14 @@ public class ArcherSpawn : MonoBehaviour {
 
 	public Rigidbody2D archer;
 
+	private bool firstRun;
+	
+	float canSpawn;
+
 	// Use this for initialization
 	void Start () {
+		firstRun = true;
+		canSpawn = Time.time + 2f;
 		//Invoke ("spawn", 1);
 	}
 	
@@ -18,12 +24,15 @@ public class ArcherSpawn : MonoBehaviour {
 	void spawn() {
 		Rigidbody2D newArcher = Instantiate(archer, this.transform.position, this.transform.rotation) as Rigidbody2D;
 			
-		Destroy(gameObject);
+		newArcher.transform.parent = this.transform;
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.tag == "Player") {
-			spawn ();
+		if((firstRun == true) && (Time.time > canSpawn)) {
+			if(other.gameObject.tag == "Player") {
+				spawn ();
+				firstRun = false;
+			}
 		}
 	}
 }
--- Assets/Scripts/AI/PatrolSpawn.cs
+++ Assets/Scripts/AI/PatrolSpawn.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class PatrolSpawn : MonoBehaviour {
 	
 	public Rigidbody2D patrol;
+
+	private bool firstRun;
+	
+	float canSpawn;
 	
 	// Use this for initialization
 	void Start () {
+		firstRun = true;
+		canSpawn = Time.time + 2f;
 		//Invoke ("spawn", 1);
 	}
 	
@@ -18,12 +24,15 @@ public class PatrolSpawn : MonoBehaviour {
 	void spawn() {
 		Rigidbody2D newPatrol = Instantiate(patrol, this.transform.position, this.transform.rotation) as Rigidbody2D;
 
-		Destroy(gameObject);
+		newPatrol.transform.parent = this.transform;
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.tag == "Player") {
-			spawn ();
+		if((firstRun == true) && (Time.time > canSpawn)) {
+			if(other.gameObject.tag == "Player") {
+				spawn ();
+				firstRun = false;
+			}
 		}
 	}
 }
